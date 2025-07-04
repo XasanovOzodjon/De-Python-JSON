3fdd4d70dba953ad1ae21ae7198240a01556070e
Ozodjon <Ozodj.one.pro@gmail.com>
2025-07-04 07:03:53 +0500

    1

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,10 @@
 
+import json
+
+with open('students.json', 'r') as file:
+    students = json.load(file)
+
+students.sort(key=lambda x: x['score'], reverse=True)
+
+with open('rating.json', 'w') as file:
+    json.dump(students, file, indent=4)
